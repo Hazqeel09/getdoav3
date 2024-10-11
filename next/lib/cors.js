54210e25b86b5c9ad99543b44217af3060bbe278
Hazqeel <hazqeel9@gmail.com>
2024-10-11 18:43:35 +0800

    feat: add first api for sedekahje

---
+++ next/lib/cors.js
@@ -0,0 +1,25 @@
+import Cors from 'cors';
+
+export const cors = Cors({
+  methods: ['GET', 'POST', 'PUT'],
+  origin: function (origin, callback) {
+    const allowedDomains = ['https://getdoa.com', 'https://sedekahje.com'];
+
+    if (allowedDomains.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+});
+
+export function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
