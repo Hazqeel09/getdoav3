c05529c3564ba72509678c3329416b6f5a313bf4
nrmnqdds <quddusnuriman@gmail.com>
2025-01-01 02:40:25 +0800

    :bug: fix prisma issue

--- remix/app/libs/prisma.server.ts
+++ remix/app/libs/prisma.server.ts
@@ -1,4 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
+//
+// const prisma = new PrismaClient()
+// export default prisma
 
-const prisma = new PrismaClient()
-export default prisma
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+	var client: PrismaClient | undefined;
+}
+
+const prisma = global.client || new PrismaClient();
+
+if (process.env.NODE_ENV === "development") global.client = client;
+
+export default prisma;
